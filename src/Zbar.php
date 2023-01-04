c6ecddd6c3c29ebef582025bc106b17d498c5754
frkcn <faarukcan@gmail.com>
2023-01-04 22:10:18 +0300

    run process method once and return output if it is not empty

--- src/Zbar.php
+++ src/Zbar.php
@@ -10,6 +10,11 @@ use TarfinLabs\ZbarPhp\Exceptions\ZbarError;
 class Zbar
 {
     /**
+     * @var Process
+     */
+    protected $process;
+
+    /**
      * @var object
      */
     protected $output;
@@ -34,7 +39,6 @@ class Zbar
      *
      * @throws InvalidFormat
      * @throws UnableToOpen
-     * @throws \TarfinLabs\ZbarPhp\Exceptions\ZbarError
      */
     public function __construct($image)
     {
@@ -49,41 +53,50 @@ class Zbar
         }
 
         $this->process = new Process(['zbarimg', '-q', '--xml', $image]);
-        $this->runProcess();
     }
 
     /**
      * Run process and assign object data to output.
      *
-     * @return void
+     * @return object
      *
      * @throws \TarfinLabs\ZbarPhp\Exceptions\ZbarError
      */
     private function runProcess()
     {
+        if (! empty($this->output)) {
+            return $this->output;
+        }
+
         $this->process->run();
 
         if (! $this->process->isSuccessful()) {
             throw ZbarError::exitStatus($this->process->getExitCode());
         }
 
-        $this->output = $this->parse($this->process->getOutput());
+        return $this->output = $this->parse($this->process->getOutput());
     }
 
     /**
      * Scan bar-code and return value.
      *
      * @return string
+     *
+     * @throws \TarfinLabs\ZbarPhp\Exceptions\ZbarError
      */
     public function scan()
     {
-        return $this->output->data;
+        $output = $this->runProcess();
+
+        return $output->data;
     }
 
     /**
      * Get the bar-code type after scanning it.
      *
      * @return string
+     *
+     * @throws \TarfinLabs\ZbarPhp\Exceptions\ZbarError
      */
     public function type()
     {
@@ -94,11 +107,14 @@ class Zbar
      * Find both the bar-code and type of the bar-code then returns an object.
      *
      * @return BarCode
+     *
+     * @throws \TarfinLabs\ZbarPhp\Exceptions\ZbarError
      */
     public function decode()
     {
-        $code = $this->output->data;
-        $type = $this->output->{'@attributes'}->type;
+        $output = $this->runProcess();
+        $code = $output->data;
+        $type = $output->{'@attributes'}->type;
 
         return new BarCode($code, $type);
     }
